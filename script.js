85ddf01af60d39c7db0f4342e9212ce583146cf0
blacksnowmartin <martinmwendwa005@gmail.com>
2025-06-24 16:51:05 +0300

    created an Ui using code gpt

---
+++ script.js
@@ -0,0 +1,6 @@
+// This file can be used for future JavaScript interactions, such as:
+// - Handling quiz logic for skin type selection
+// - Implementing smooth scrolling for navigation links
+// - Adding animations or dynamic content loading
+
+console.log("script.js loaded: Ready for future enhancements!");
